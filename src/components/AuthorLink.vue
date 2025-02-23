1eb8b7597c2456506ee61b86996d2f144e96ee77
Skyawesome73 <skyawesome73@gmail.com>
2025-02-23 14:37:34 -0600

    Built views and configured routes

--- src/components/AuthorLink.vue
+++ src/components/AuthorLink.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { computed } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const props = defineProps(['author']);
+const firstName = props.author.user.firstName;
+const lastName = props.author.user.lastName;
+const displayName = computed(() => {
+    if (firstName && lastName) {
+        return `${firstName} ${lastName}`;
+    } else {
+        return `${props.author.user.username}`;
+    }
+});
+</script>
+
 <template>
-    <h3>Author Link</h3>
+    <RouterLink 
+    :to="{ name: 'author', params: { username: author.user.uesrname } }">
+        {{ displayName }}
+    </RouterLink>
 </template>
+
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
